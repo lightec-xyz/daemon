73d40693f58177b10b92e8d6a899d51e8d95ed23
red <406567072@qq.com>
2024-07-03 14:44:39 +0800

    update

--- rpc/ws/handler.go
+++ rpc/ws/handler.go
@@ -1,11 +1,14 @@
 package ws
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"unicode"
 )
 
 // todo
+// just a simple test handler
 
 type handler struct {
 	calls map[string]*call
@@ -13,68 +16,148 @@ type handler struct {
 
 func newHandler(h interface{}) *handler {
 	calls := make(map[string]*call)
-	rTypes := reflect.TypeOf(h)
-	rValues := reflect.ValueOf(h)
-	for i := 0; i < rTypes.NumMethod(); i++ {
-		method := rTypes.Method(i)
-		calls[method.Name] = &call{
-			name:   method.Name,
-			method: method,
-			root:   rValues,
+	receiver := reflect.ValueOf(h)
+	typ := receiver.Type()
+	for m := 0; m < typ.NumMethod(); m++ {
+		method := typ.Method(m)
+		if method.PkgPath != "" {
+			continue
 		}
+		name := formatName(method.Name)
+		cb := newCall(name, receiver, method.Func)
+		if cb == nil {
+			continue
+		}
+		calls[name] = cb
 	}
+
 	return &handler{
 		calls: calls,
 	}
 }
 
-func (h *handler) call(name string) interface{} {
-	return h.calls[name].call(nil)
+func (h *handler) call(name string, args ...interface{}) (interface{}, error) {
+	//argsByte, err := json.Marshal(args)
+	//if err != nil {
+	//	return nil, err
+	//}
+	call, ok := h.calls[name]
+	if !ok {
+		return nil, fmt.Errorf("no such method: %s", name)
+	}
+	result, err := call.call(nil)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 type call struct {
-	name   string
-	args   []reflect.Value
-	method reflect.Method
-	root   reflect.Value
+	name     string
+	fn       reflect.Value  // the function
+	rcvr     reflect.Value  // receiver object of method, set if fn is method
+	argTypes []reflect.Type // input argument types
 }
 
-func (c *call) call(obj interface{}) interface{} {
-	reqType := c.method.Type.In(1)
-	reqValue := reflect.New(reqType).Elem()
-	for i := 0; i < reqType.NumField(); i++ {
-		//field := reqType.Field(i)
-		fieldValue := reqValue.Field(i)
-		if fieldValue.CanSet() {
-			switch fieldValue.Kind() {
-			case reflect.Int:
-				fieldValue.SetInt(1) // 动态设置为你需要的值
-			case reflect.String:
-				fieldValue.SetString("example")
-				// 可以根据需要添加其他类型的处理
-			}
+func (c *call) call(obj []byte) (interface{}, error) {
+	//args, err := parseJsonToArgs(json.NewDecoder(bytes.NewReader(obj)), c.argTypes)
+	//if err != nil {
+	//	return nil, err
+	//}
+	var args []reflect.Value
+	args = append(args, c.rcvr)
+	out := c.fn.Call(args)
+	if len(out) == 0 {
+		return nil, nil
+	} else if len(out) == 1 {
+		return nil, out[0].Interface().(error)
+	} else if len(out) == 2 {
+		if out[1].IsNil() {
+			return out[0], nil
 		}
+		return out[0], out[1].Interface().(error)
+	} else {
+		return nil, fmt.Errorf("return value more than 2")
 	}
-	out := c.method.Func.Call([]reflect.Value{c.root, reqValue})
-	responseValue := out[0]
-	if responseValue.Kind() == reflect.Ptr {
-		responseValue = responseValue.Elem()
+}
+func (c *call) makeArgTypes() {
+	fntype := c.fn.Type()
+	// Skip receiver and context.Context parameter (if present).
+	firstArg := 0
+	if c.rcvr.IsValid() {
+		firstArg++
 	}
-	responseType := responseValue.Type()
+	// Add all remaining parameters.
+	c.argTypes = make([]reflect.Type, fntype.NumIn()-firstArg)
+	for i := firstArg; i < fntype.NumIn(); i++ {
+		c.argTypes[i-firstArg] = fntype.In(i)
+	}
+}
 
-	fmt.Println("Response Fields:")
-	for i := 0; i < responseType.NumField(); i++ {
-		field := responseType.Field(i)
-		fieldValue := responseValue.Field(i)
-		fmt.Printf("%s: %v\n", field.Name, fieldValue.Interface())
-	}
+func newCall(name string, receiver, fn reflect.Value) *call {
+	c := &call{name: name, fn: fn, rcvr: receiver}
+	c.makeArgTypes()
+	return c
+}
 
-	// 处理error
-	errValue := out[1]
-	if !errValue.IsNil() {
-		err := errValue.Interface().(error)
-		fmt.Println("Error:", err)
+type testHandler struct {
+	name  string
+	count int
+}
 
+func (t *testHandler) Version(req Request) (*Response, error) {
+	fmt.Printf("version req: %v", req)
+	return &Response{"Success"}, nil
+}
+
+func (t *testHandler) Add(name string, count int) (string, error) {
+	t.name = name
+	t.count = count
+	return "", nil
+}
+
+func (t *testHandler) Info() (string, error) {
+	info := fmt.Sprintf("info %v:%v", t.name, t.count)
+	return info, nil
+}
+
+func (t *testHandler) Test() error {
+	fmt.Printf("test: \n")
+	return nil
+}
+
+type Request struct {
+	Height int
+}
+
+type Response struct {
+	Msg string
+}
+
+func formatName(name string) string {
+	ret := []rune(name)
+	if len(ret) > 0 {
+		ret[0] = unicode.ToLower(ret[0])
 	}
-	return out[0].Interface()
+	return string(ret)
+}
+
+func parseJsonToArgs(dec *json.Decoder, types []reflect.Type) ([]reflect.Value, error) {
+	args := make([]reflect.Value, 0, len(types))
+	for i := 0; dec.More(); i++ {
+		if i >= len(types) {
+			return args, fmt.Errorf("too many arguments, want at most %d", len(types))
+		}
+		argval := reflect.New(types[i])
+		if err := dec.Decode(argval.Interface()); err != nil {
+			return args, fmt.Errorf("invalid argument %d: %v", i, err)
+		}
+		if argval.IsNil() && types[i].Kind() != reflect.Ptr {
+			return args, fmt.Errorf("missing value for required argument %d", i)
+		}
+		args = append(args, argval.Elem())
+	}
+	// Read end of args array.
+	_, err := dec.Token()
+	return args, err
 }
--- rpc/ws/handler_test.go
+++ rpc/ws/handler_test.go
@@ -1,14 +1,33 @@
 package ws
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+)
 
 func TestHandler(t *testing.T) {
-	node := Node{}
+	node := testHandler{}
 	handler := newHandler(&node)
-	result := handler.call("Version")
-	t.Log(result)
-	t.Log(node.Count)
-	result = handler.call("Version")
-	t.Log(node.Count)
+
+	info, err := handler.call("info")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(info)
+
+}
+
+func TestDemo(t *testing.T) {
+	data := []byte(`{"name":"able","age":10}`)
+	//data := []byte(`[1,"age",3,4,5,6,"able"]`)
+	decoder := json.NewDecoder(bytes.NewReader(data))
+	for decoder.More() {
+		token, err := decoder.Token()
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(token)
+	}
 
 }
