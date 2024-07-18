5b6da47b900d4d422a116ea475d05dbed5add5c3
red <406567072@qq.com>
2024-07-18 19:01:42 +0800

    update

--- node/utils_test.go
+++ node/utils_test.go
@@ -1,9 +1,27 @@
 package node
 
-import "testing"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"github.com/ethereum/go-ethereum/crypto"
+	"testing"
+)
 
-func TestTxIdIsEmpty(t *testing.T) {
-	txId := [32]byte{}
-	res := TxIdIsEmpty(txId)
-	t.Log(res)
+func TestVerifySignature(t *testing.T) {
+	publicKey, err := hex.DecodeString("02a3f345daecb93c23528849817a00762d611f385e84549974aec7c62ee8cae987")
+	if err != nil {
+		t.Fatal(err)
+	}
+	signature, err := hex.DecodeString("50eae8431e89a15bca4d0e72e92183dd2b4530d3e5efa6ba27938d771aa0ac5947ef5a1cbe9ea30e1da0f291465fa0369f4f8d108e8387946332adced8dbc9ae")
+	if err != nil {
+		t.Fatal(err)
+	}
+	verify := VerifySha256Signature(publicKey, signature, []byte("0000000000000007fc451dcb79d027d52c0cdd3f5a3e19f230a435dc72ad6a65"))
+	t.Log(verify)
+}
+
+func VerifySha256Signature(publicKey []byte, signature []byte, data []byte) bool {
+	hash := sha256.Sum256(data)
+	verified := crypto.VerifySignature(publicKey, hash[:], signature)
+	return verified
 }
