0a628e2875513a4db1b1b37f0c259ac98fde8625
red <406567072@qq.com>
2024-08-06 17:40:28 +0800

    update

--- cmd/generator/cmd/run.go
+++ cmd/generator/cmd/run.go
@@ -3,9 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/lightec-xyz/daemon/common"
 	"github.com/lightec-xyz/daemon/proof"
-	"github.com/lightec-xyz/daemon/rpc"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -27,22 +25,6 @@ var runCmd = &cobra.Command{
 			fmt.Printf("unmarshal config error: %v %v \n", cfgFile, err)
 			return
 		}
-
-		client, err := rpc.NewNodeClient("https://testnet.zkbtc.money/api")
-		if err != nil {
-			fmt.Printf("new client error: %v %v \n", cfgFile, err)
-			return
-		}
-		info, err := client.ProofInfo([]string{"0x4438c9e843b35e549173658a1409c4577ad78dae5b2cda70008cb31a541c4458"})
-		if err != nil {
-			fmt.Printf("proof info error: %v %v \n", cfgFile, err)
-			return
-		}
-		fmt.Printf("proof info: %v \n", info)
-		zkProofTypes := common.GetEnvZkProofTypes()
-		fmt.Printf("zk proof types: %v \n", zkProofTypes)
-		return
-
 		node, err := proof.NewNode(config)
 		if err != nil {
 			fmt.Printf("new node error: %v %v \n", cfgFile, err)
