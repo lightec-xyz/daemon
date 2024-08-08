4b80a956e40d54afbd37f9783d2927f239f5ef0a
red <406567072@qq.com>
2024-08-08 23:15:40 +0800

    fix bug

--- proof/local.go
+++ proof/local.go
@@ -71,8 +71,8 @@ func (l *Local) Run() error {
 	}
 	go func(request *common.ZkProofRequest) {
 		count := 0
+		defer l.worker.DelReqNum()
 		for {
-			defer l.worker.DelReqNum()
 			if count >= 1 { // todo
 				logger.Error("retry gen proof too much time,stop generate this proof now: %v", request.Id())
 				return
