9db2b4b0dda9c9cb46c822f51d8c910ed4413cab
Red <45277734+ltp456@users.noreply.github.com>
2025-06-25 17:29:02 +0800

    update

--- node/queue.go
+++ node/queue.go
@@ -65,11 +65,12 @@ func (q *PendingQueue) Iterator(fn func(value *common.ProofRequest) error) {
 }
 
 func sortRequest(a, b *common.ProofRequest) bool {
-	if common.IsBtcInternalProofType(a.ProofType) && common.IsBtcInternalProofType(b.ProofType) {
-		return a.FIndex < b.FIndex
-	}
+	//todo 
 	if a.Weight == b.Weight {
-		if a.ProofType == b.ProofType { // todo more rule
+		if common.IsBtcInternalProofType(a.ProofType) && common.IsBtcInternalProofType(b.ProofType) {
+			return a.FIndex < b.FIndex
+		}
+		if a.ProofType == b.ProofType {
 			if a.ProofType == common.SyncComInnerType {
 				if a.Prefix == b.Prefix {
 					return a.FIndex < b.FIndex
