a3500ef36c9a584e6cd83e7f93266d0c5f52168f
Red <45277734+ltp456@users.noreply.github.com>
2025-06-25 12:59:33 +0800

    update

--- cmd/node/cmd/init.go
+++ cmd/node/cmd/init.go
@@ -4,9 +4,8 @@ import (
 	"github.com/lightec-xyz/daemon/cmd/node/cmd/miner"
 	"github.com/lightec-xyz/daemon/cmd/node/cmd/proof"
 )
-import _ "github.com/lightec-xyz/daemon/cmd/node/cmd/proof"
 
 func init() {
 	rootCmd.AddCommand(miner.MinerCmd)
-	rootCmd.AddCommand(storage.ProofCmd)
+	rootCmd.AddCommand(proof.ProofCmd)
 }
