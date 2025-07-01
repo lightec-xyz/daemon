0e27239bd5b46f4a288c95a049a057a91dedc655
Red <45277734+ltp456@users.noreply.github.com>
2025-07-01 09:26:50 +0800

    update

--- common/common_test.go
+++ common/common_test.go
@@ -3,6 +3,7 @@ package common
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func Test_Reverse(t *testing.T) {
@@ -17,4 +18,13 @@ func TestBtcChainGenesisIndex(t *testing.T) {
 	cpStartIndex := endIndex - 200
 	fmt.Printf("checkPointStartIndex: %d \n", cpStartIndex)
 
+}
+func TestStrToTime(t *testing.T) {
+	str := "2025-07-01 03:58:55"
+	ti, err := time.Parse("2006-01-02 15:04:05", str)
+	if err != nil {
+		fmt.Println("parse error:", err)
+		return
+	}
+	t.Log(ti.Unix())
 }
