54d637fa6679cdbfd415e2a01630a1515594c145
Weiji Guo <weiji.g@gmail.com>
2026-01-08 14:12:34 +0800

    updated BtxTxVerifier address

--- node/constant_testnet.go
+++ node/constant_testnet.go
@@ -8,7 +8,7 @@ const (
 	TestnetEthZkBridgeAddress    = "0x9C83D62f524d589da2ED538F5F59225880B680D1"
 	TestnetEthZkBtcAddress       = "0x71198dD1689eBc01A7664ce7c0d5b1f62DF2b378"
 	TestnetEthUtxoManagerAddress = "0xc5eC55Ba3764c09eD28598465D534e313A0EEe34"
-	TestnetEthBtcTxVerifyAddress = "0x6e4abEe4d5D97Ce08Da9CBb050f46677d1e23fDd"
+	TestnetEthBtcTxVerifyAddress = "0xC96685Eb518Df9959E326326d72b5234b78c2d30"
 	TestnetFeePoolAddr           = "0xd69aec9e9a434aa6B0a9BCC9B4D01154fc907253"
 	TestnetOasisSignerAddr       = "0x78131d6524C04F986e82Cd222a6ABcBf8f9D9Dc1"
 	TestnetIcpPublicKey          = "0x02b4ca925faa4e78c292c080254121299a778e990dd52061fb6d5dd01ef9db56f2"
